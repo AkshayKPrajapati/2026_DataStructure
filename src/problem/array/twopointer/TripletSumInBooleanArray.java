5b2264264e7969fecbdfa7646eb0962622801de6
AkshayKPrajapati <akshaykumarprajapati@zohomail.in>
2026-08-12 23:51:03 +0530

    three sum

--- src/problem/array/twopointer/TripletSumInBooleanArray.java
+++ src/problem/array/twopointer/TripletSumInBooleanArray.java
@@ -1,7 +1,6 @@
 package problem.array.twopointer;
 
-import javax.net.ssl.SSLEngineResult.HandshakeStatus;
-
+import java.util.HashSet;
 public class TripletSumInBooleanArray {
 	public static void main(String[] args) {
 		int arr[] = {1, 4, 45, 6, 10, 8} ;
@@ -9,10 +8,12 @@ public class TripletSumInBooleanArray {
 		boolean x = hasTripletSum(arr,target);
 		System.out.println(x);
 	}
+	
+	//my code 
 	public static boolean hasTripletSum(int arr[], int target) {
 		int n=arr.length;
 		for(int i=0;i<n;i++) {
-			for(int j=i+i;j<n;j++) {
+			for(int j=i+1;j<n;j++) {
 				for(int k=j+1;k<n;k++) {
 					int sum=arr[i]+arr[j]+arr[k];
 					
@@ -23,7 +24,27 @@ public class TripletSumInBooleanArray {
 			}
 		}
 		return false;
-        // code Here
         
+    }
+	
+	//optimization code
+	public static boolean hasTripletSums(int arr[], int target) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            HashSet<Integer> set = new HashSet<>();
+
+            for (int j = i + 1; j < n; j++) {
+                int required = target - arr[i] - arr[j];
+
+                if (set.contains(required)) {
+                    return true;
+                }
+
+                set.add(arr[j]);
+            }
+        }
+
+        return false;
     }
 }
